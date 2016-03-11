ce4d67680d61bd0b605664585cbdee1c6389b958
Shivam Mishra <shivam@indeed.com>
2016-03-11 20:32:17 +0530

    updated with log statement

--- iguana/js/corenav.js
+++ iguana/js/corenav.js
@@ -1,22 +1,22 @@
 
 	var rel = "BTC";
     var data = [{'coin': "NXT" , 'volume': 20000000},{'coin': "VPN" , 'volume': 20000000},{'coin': "VRC" , 'volume': 2000000},{'coin': "BTCD" , 'volume': 50000},{'coin': "SYS" , 'volume': 1000000}];
-    
     var coreNav = 0;
-
-    var apiRequestForEachCoin = function(i){
+	
+	var apiRequestForEachCoin = function(i){
 		if(i<5){
 			var request="{\"agent\":\"tradebot\",\"method\":\"aveprice\",\"base\":\""+data[i].coin+"\",\"rel\":\""+rel+"\",\"basevolume\":\""+data[i].volume+"\"}";
 		      	SPNAPI.makeRequest(request, function(request,response){
 		            response=JSON.parse(response);
 		            if(response.result){
-		             	this.coreNav = this.coreNav + (response.aveprice * (data[i]).volume)
+		            	coreNav = coreNav + (response.aveprice * (data[i]).volume)
 		            }
 		            if(i >= (data.length-1)){
-					    this.coreNav = this.coreNav/816061.0;
+					    coreNav = coreNav/816061.0;
 					    i = 0;
-					    document.getElementById("coreNav").innerHTML= "<b>coreNAV: "+this.coreNav + " BTC</b>";
-					    this.coreNav = 0
+					    document.getElementById("coreNav").innerHTML= "<b>coreNAV: "+coreNav + " BTC</b>";
+					    console.log("final calculated coreNav", coreNav)
+					    coreNav = 0
 			   		}else{
 			   			i += 1;
 		             	apiRequestForEachCoin(i);
@@ -25,4 +25,4 @@
 			}
 		}
    
-   var refreshId = setInterval(function(){apiRequestForEachCoin(0);}, 10000);	
+    var refreshId = setInterval(function(){apiRequestForEachCoin(0);}, 10000);	
