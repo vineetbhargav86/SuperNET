e47119f0588cd93447c3407d19235fc09064df16
Shivam Mishra <shivam@indeed.com>
2016-03-11 20:23:20 +0530

    coreNAV bug fix

--- iguana/js/corenav.js
+++ iguana/js/corenav.js
@@ -3,29 +3,26 @@
     var data = [{'coin': "NXT" , 'volume': 20000000},{'coin': "VPN" , 'volume': 20000000},{'coin': "VRC" , 'volume': 2000000},{'coin': "BTCD" , 'volume': 50000},{'coin': "SYS" , 'volume': 1000000}];
     
     var coreNav = 0;
-	var callback = function(i){
-		var request="{\"agent\":\"tradebot\",\"method\":\"aveprice\",\"base\":\""+data[i].coin+"\",\"rel\":\""+rel+"\",\"basevolume\":\""+data[i].volume+"\"}";
-	      SPNAPI.makeRequest(request, function(request,response,coreNav){
-	            response=JSON.parse(response);
-	            if(response.result){
-	            	console.log('Before coreNav' , this.coreNav);
-	             	this.coreNav = this.coreNav + (response.aveprice * data[i].volume)
-	             	console.log("After coreNav",  this.coreNav);
-	            }
-	       });
-	      if(this.coreNav > 0 && i == (data.length-1)){
-				    this.coreNav = this.coreNav/816061.0;
-				    console.log('Final coreNav' , this.coreNav);
-		   }
-		   document.getElementById("coreNavV").innerHTML= "<b>coreNAV: "+this.coreNav + " BTC</b>";
-	}
-	
-	function getNav(){
-		for( var i in data){
-	      callback(i);
-	    }
-	    coreNav = 0;
-	};
-	
 
-   var refreshId = setInterval(getNav, 10000);
+    var apiRequestForEachCoin = function(i){
+		if(i<5){
+			var request="{\"agent\":\"tradebot\",\"method\":\"aveprice\",\"base\":\""+data[i].coin+"\",\"rel\":\""+rel+"\",\"basevolume\":\""+data[i].volume+"\"}";
+		      	SPNAPI.makeRequest(request, function(request,response){
+		            response=JSON.parse(response);
+		            if(response.result){
+		             	this.coreNav = this.coreNav + (response.aveprice * (data[i]).volume)
+		            }
+		            if(i >= (data.length-1)){
+					    this.coreNav = this.coreNav/816061.0;
+					    i = 0;
+					    document.getElementById("coreNav").innerHTML= "<b>coreNAV: "+this.coreNav + " BTC</b>";
+					    this.coreNav = 0
+			   		}else{
+			   			i += 1;
+		             	apiRequestForEachCoin(i);
+			   		}
+			   });
+			}
+		}
+   
+   var refreshId = setInterval(function(){apiRequestForEachCoin(0);}, 10000);	
